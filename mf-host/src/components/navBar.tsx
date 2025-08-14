839f9b2169008bc185ff85db208106dbcdac1823
CesarArt <arturo_inter8@hotmail.com>
2025-08-14 14:33:11 -0600

    🚧 Add Navbar

---
+++ mf-host/src/components/navBar.tsx
@@ -0,0 +1,34 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import useStorageToken from "../custom-hooks/use-storage-token";
+import { LogOut } from 'lucide-react';
+
+export default function Navbar() {
+    const { removeUserToken } = useStorageToken();
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const signOutAction = () => {
+        removeUserToken();
+        if (location.pathname === "/") {
+            window.location.reload();
+        } else {
+            navigate("/")
+        }
+    }
+
+    return (
+        <nav className="flex sticky w-full bg-white text-black h-18 px-8 justify-between">
+            {/* Left */}
+            <div className="flex gap-2 items-center">
+                <p className="text-xl">Rick and Morty Universe</p>
+            </div>
+            {/* Right */}
+            <div className="flex gap-2 items-center">
+                <span className="flex gap-2 items-center cursor-pointer p-2 rounded-2xl bg-zinc-100 hover:opacity-70" onClick={signOutAction}>
+                    <p className="text-sm text-gray-600">Log Out</p>
+                    <LogOut size={14} />
+                </span>
+            </div>
+        </nav>
+    );
+}
