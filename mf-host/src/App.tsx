ee1539e7fa52c9c125ae004946d26a6030fc2462
CesarArt <arturo_inter8@hotmail.com>
2025-08-14 14:32:53 -0600

    🚧 Add sonner Toaster to global notifications

--- mf-host/src/App.tsx
+++ mf-host/src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "sonner"
 import { MainLayoutPage } from "./pages/mainLayout"
 
 function App() {
 
   return (
     <div className="bg-gray-100 w-full h-screen">
+      <Toaster position="top-center" richColors closeButton/>
       <MainLayoutPage />
     </div>
   )
