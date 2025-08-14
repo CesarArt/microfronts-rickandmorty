1e19498567e7da349818c7cddc95f9ca33099a49
CesarArt <arturo_inter8@hotmail.com>
2025-08-14 14:18:10 -0600

    🚧 ✨ Config App page

--- mf-host/src/App.tsx
+++ mf-host/src/App.tsx
@@ -1,10 +1,10 @@
+import { MainLayoutPage } from "./pages/mainLayout"
+
 function App() {
 
   return (
-    <div className="bg-gray-300 w-full h-screen p-4">
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="bg-gray-100 w-full h-screen">
+      <MainLayoutPage />
     </div>
   )
 }
